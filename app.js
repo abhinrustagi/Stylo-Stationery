ecde9bec9995f32c7c94b91726e092c66ced3337
Abhin Rustagi <rustagi.abhin@gmail.com>
2020-09-01 17:08:56 +0530

    Add Compass, Chalk

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 const express= require("express");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 
-const app= express();
+const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-mongoose.connect("mongodb://localhost:27017/stationeryDB", {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:false});
+// mongoose.connect("mongodb://localhost:27017/stationeryDB", {useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:false});
 
 //yet to decide the schema
 
-app.get("/", (req,res)=>{
-res.sendFile(__dirname+  "/index.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/products", (req,res)=>{
-res.sendFile(__dirname+"/products.html")
+app.get("/products", (req, res) => {
+  res.sendFile(__dirname + "/products.html")
 });
 
-app.get("/know-us", (req,res)=>{
-res.sendFile(__dirname+"/knowUs.html");
+app.get("/know-us", (req, res) => {
+  res.sendFile(__dirname + "/knowUs.html");
 });
 
 
-app.listen(3000,()=>{
+app.listen(3000,() => {
   console.log("Server started on port 3000");
 })
